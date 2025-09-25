9a9e00c6789e0c6d4033a9485745ec4c0d390bd1
KaterynaBachkalo <kozlovets88@gmail.com>
2025-09-25 11:32:45 +0300

    add

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+if (process.env.NODE_ENV === 'production' && !process.env.NEXT_PUBLIC_API_URL) {
+  throw new Error('Missing NEXT_PUBLIC_API_URL for production build');
+}
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+    loader: 'default',
+  },
+  basePath: isProd ? '/crm_next_practice_front' : '',
+  assetPrefix: isProd ? '/crm_next_practice_front/' : '',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
